3751434b2c434c9bae366538016c2af6cd1d9791
Shahzad Ahmad <55791391+engineershahzad@users.noreply.github.com>
2020-06-23 23:03:16 +0500

    JS assignment

---
+++ assignment 6/app.js
@@ -0,0 +1,275 @@
+        //....    Assignment # 6 
+        //....    ch 21 to 25
+        //....    Task # 1
+
+
+// var firstName = prompt("Enter your first name");
+// var secondName = prompt("Enter your second name");
+// var fullName =  firstName + " " + secondName;
+// document.write(fullName +  " Welcome to javaScript!")
+
+
+
+
+        //........     Task # 2
+
+        // var favourit = prompt(" enter your favourite phone model");
+        // var inpLength = favourit.length
+        // document.write("Length of string :" + inpLength)
+
+
+
+
+
+        //.......     Task # 3
+
+        // var name = "Pakistani";
+        // console.log(name.indexOf("n"));
+
+
+
+
+
+        //.......    Task # 4
+
+        // var word = "Hello World";
+        // console.log(word.lastIndexOf("l"))
+
+
+
+
+
+
+        //......     Task# 5 
+
+        // var name = "Pakistani";
+        //  console.log("character at 3rd index :" + " "+name.charAt(3));
+
+
+
+
+        //.....      Task # 6
+
+
+
+
+        //.....      Task # 7
+
+        // var text = "Hyderabad";
+        // for(var i = 0; i< text.length; i++){
+        //     if(text.slice(i, i+5) === "Hyder"){
+        //         text = "Islam"+text.slice(i+5);
+                
+               
+        //     }
+        //     document.write("City : Hyderabad  <br> After replacement: "+ text)
+        //     break
+        // }
+
+
+
+
+
+
+        //......        Task #8
+
+
+        // var strng =  "Ali and Sami are best friends. They play cricket and football together.";
+
+        // var newStrng = strng.replace(/and/g, "&");
+        // document.write(newStrng)
+
+
+
+
+
+        //.....        Task # 9
+
+        // var strng = "472";
+        // var asIntgr = Number(strng);
+        // document.write("value: "+ asIntgr +" <br> type: Number <br>" ) 
+        // var asString = asIntgr.toString()
+        // document.write("value: "+asString+" <br> type: String")
+        
+
+
+        //.....      Task # 10
+
+        // var word = prompt("enter the word");
+        // word = word.toUpperCase();
+        // document.write(word)
+
+
+
+
+        //......     Task # 11
+
+
+    //     var word = prompt("enter the word");
+    //    var firstWord = word.slice(0,1);
+    //    var secondWord = word.slice(1);
+    //     firstWord =  firstWord.toUpperCase();
+    //     secondWord = secondWord.toLowerCase();
+    //     var finalWord = document.write(firstWord+secondWord) 
+    
+        
+    
+
+    //...                 Task #12
+
+
+
+//     var num = 35.36;
+//     var numString = num.toString();
+//     for(var i=0; i<=numString.length -1; i++){
+//             if( numString.charAt(i)==="."){
+//                    var newNmbr =  numString.slice(0, i) + numString.slice(3)
+//             document.write(newNmbr)
+//             }
+//     }
+
+
+
+        //......       Task # 13 A
+
+
+        // var userName = prompt("enter user name");
+
+        // if(/^[a-zA-Z0-9- #$%*()?]*$/.test(userName) == false){
+        //         alert("please enter valid name")
+        //     }else{
+        //             document.write(userName+" welcome to javaScript")        //  Help taken from
+                                                                                //  www.sitepoint.com
+        //     }
+
+
+
+        //......      Task # 13 B
+
+
+        // var userName = prompt("enter user name");
+        // for(var i=0; i<= userName.length-1; i++){
+        //         if(userName.charCodeAt(userName.length-1) === 33 || userName.charCodeAt(userName.length-1) === 44 ||
+        //         userName.charCodeAt(userName.length-1) === 46 || userName.charCodeAt(userName.length-1) === 64){
+        //                 alert("please enter valid user name")
+        //         }else
+        //         document.write("welcome "+ userName)
+        //         break
+        // }
+
+
+
+
+
+        //......       Task # 14
+
+
+        // var baKery = ["cake", "apple", "pie", "cookie", "chips", "patties"] ;
+        // var userSearch = prompt("welcome to ABC Bakery. What do you want to order \n sir/ma'am");
+        // userSearch = userSearch.toLowerCase();
+        // for(var i=0; i<=baKery.length-1; i++){
+        //         if(userSearch===baKery[i]){
+        //                 alert(userSearch+ " is available  at index "+ i + " in our bakery");
+                
+
+        //         }
+                
+
+        // }  if() alert("We are sorry. " +userSearch+ "is not available in our Bakery")  //.... i stucked here
+        
+
+
+
+        //.......    Task # 15
+
+
+        // var passWord = prompt("enter password");
+        // for(var i=0; i<= passWord.length-1; i++ ){
+
+        //  if(/^[a-zA-Z0-9- #$%*()?]*$/.test(passWord) == false && passWord.length !== 6 ){
+        //          alert("please enter valid name");
+                 
+
+        //  }else{
+                
+        //         document.write(passWord)
+        //         break
+
+        //  }
+        
+        
+        // }                                            //     stucked
+
+
+
+
+        //.......     Task # 16
+
+        //  var university = "University of Karachi";
+        //  var uniArray = university.split("");
+        //  for(var i = 0; i<= uniArray.length-1; i++){
+        //          document.write(uniArray[i] +"<br>" )
+                 
+        //  }
+         
+
+
+
+
+        //.......    Task # 17
+
+        // var word = prompt("enter the word");
+        // var wordLength = word.length;
+        
+        //                 document.write("Last character of input: " + word.charAt(wordLength-1))
+                
+        
+
+
+
+
+        //                     ch # 25 to ch # 30
+
+        //......    Task # 1
+
+
+        // var nmber = +prompt( "enter the float");
+        // document.write(nmber + "<br>");
+        // var roundNmbr = Math.round(nmber);
+        // document.write(roundNmbr + "<br>");
+        // var ceilNmbr = Math.ceil(nmber);
+        // document.write(ceilNmbr + "<br>");
+        // var floorNmbr = Math.floor(nmber);
+        // document.write(floorNmbr + "<br>")
+
+
+
+        //......   Task # 2
+
+
+        // var nmber = -prompt("enter the float");
+        // document.write(nmber + "<br>");
+        //  var roundNmbr = Math.round(nmber);
+        //  document.write(roundNmbr + "<br>");
+        //  var ceilNmbr = Math.ceil(nmber);
+        //  document.write(ceilNmbr + "<br>");
+        //  var floorNmbr = Math.floor(nmber);
+        //  document.write(floorNmbr + "<br>")
+
+        
+
+
+        //.....    Task # 3
+
+
+        // var decimlNmbr = Math.random();
+        // var secondNmbr = (decimlNmbr*6) + 1;
+        // var finalNmbr = Math.ceil(secondNmbr);
+        // document.write(finalNmbr)
+
+
+
+
+
+        //..... Task # 4
+
